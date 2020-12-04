7e8110e2a0a4f7bb9073ccd5041a74366958b043
joralsiz95 <joralsiz95@gmail.com>
2020-12-04 18:57:21 -0500

    Agregando README

---
+++ README.md
@@ -0,0 +1,27 @@
+## Maquina de Turing
+
+AppWeb que implementa el modelo de Turing para validar entradas a un programa escrito con JavaScript, utilizando React js.
+
+## Landing page del proyecto:
+
+![Landing](https://github.com/joralsiz95/maquinaTuring/blob/main/src/imagenes/screen1.png?raw=true)
+
+## Página con proceso en modo manual:
+
+![manual](https://github.com/joralsiz95/maquinaTuring/blob/main/src/imagenes/screen2.png?raw=true)
+
+## Página con proceso en modo automático:
+
+![automatica](https://github.com/joralsiz95/maquinaTuring/blob/main/src/imagenes/screen3.png?raw=true)
+
+## Link Pagina web
+ * 
+ 
+## Creado por:
+ * Sebastian Troncoso Correa
+ * Jorge Alberto Silva Zambrano
+ * Andres Felipe Brieva Pinedo
+ * Luis Eduardo Gamez Maldonado
+ 
+## Profesor:
+ * ING. Daniel Gonzales Polo, UNIMAGDALENA
--- src/App.js
+++ src/App.js
@@ -21,13 +21,13 @@ function App() {
       html: <p> <strong>Te damos la bienvenida a este simulador de la Maquina de Turing</strong> <br/><br/>
             
             Este simulador tiene un caso de uso especifico:<br/><br/>
-            ➤ Te permitira ingresar una cadena con combinaciones cualesquiera de a y b en minúscula. <br/><br/>
+            ➤ Te permitirá ingresar una cadena con combinaciones cualesquiera de a y b en minúscula. <br/><br/>
             ➤ Luego debes cargar la máquina para la simulación (La cadena que escribiste se ubicara en la cinta). <br/><br/>
-            ➤ Posterior a ello tendras que seleccionar el modo en que deseas ver el proceso, puedes hacerlo paso a paso 
-            de forma manual o puedes dejar que la simulación sea automática, en caso de que optes por esta ultima opción 
-            tendras que establecer una velocidad de simulación en el slider, la máxima son 300 milisegundos y la mínima son 1000.<br/><br/> 
+            ➤ Posterior a ello tendrás que seleccionar el modo en que deseas ver el proceso, puedes hacerlo paso a paso 
+            de forma manual o puedes dejar que la simulación sea automática, en caso de que optes por esta última opción 
+            tendrás que establecer una velocidad de simulación en el slider, la máxima son 300 milisegundos y la mínima son 1000.<br/><br/> 
             ➤ Puedes cambiar de modo automático a manual y viceversa. <br/><br/>
-            ➤ Al dar click en los botones de "iniciar" y "siguiente" de cada modo podras notar que la simulación convierte la cadena de 
+            ➤ Al dar click en los botones de "iniciar" y "siguiente" de cada modo podrás notar que la simulación convierte la cadena de 
             <strong> a</strong>es y <strong>b</strong>es que escribiste en una cadena que solo contiene <strong>a</strong>es a medida
             que ocurre magia en el grafo y en la cinta.<br/><br/>
 
